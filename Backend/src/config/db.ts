aba28f1fb61a9a03c4467102e2c84e2c7ec5793a
harry-sandhu <120314298+harry-sandhu@users.noreply.github.com>
2026-04-20 08:45:51 +0000

    backedn env ddfdfdxdsfsdsdf

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -4,18 +4,33 @@ import { config } from "./config";
 let cached = (global as any).mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
-export const connectDB = async (): Promise<void> => {
-  if (cached.conn) return;
-   console.log("MONGO_URI:", config.mongoUri);
+export const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+
   if (!cached.promise) {
-    cached.promise = mongoose.connect(config.mongoUri).then((mongoose) => {
-      console.log("✅ MongoDB connected");
-      return mongoose;
-    });
+    console.log("🔥 Connecting to Mongo...");
+
+    cached.promise = mongoose
+      .connect(config.mongoUri, {
+        serverSelectionTimeoutMS: 5000,
+      })
+      .then((mongoose) => {
+        console.log("✅ MongoDB connected");
+        return mongoose;
+      })
+      .catch((err) => {
+        console.error("❌ Mongo connection failed:", err);
+        cached.promise = null; // 🔥 IMPORTANT: allow retry
+        throw err;
+      });
   }
 
   cached.conn = await cached.promise;
+  return cached.conn;
 };
