6e54fbfe904635f59cd26f1799a29b4da119725b
harry-sandhu <120314298+harry-sandhu@users.noreply.github.com>
2026-04-20 09:19:26 +0000

    fix cold start mongoosdsfdsfsd hjjhgchfh

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,18 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app";
-import { connectDB } from "./config/db";
 import { config } from "./config/config";
 
-// Connect to database
-connectDB();
+// ❌ NO connectDB here
 
-// Start server ONLY if not running on Vercel
 if (!process.env.VERCEL) {
   app.listen(config.port || 3000, () => {
     console.log(`🚀 Server running on port ${config.port || 3000}`);
   });
 }
 
-// Export for Vercel serverless
 export default app;
